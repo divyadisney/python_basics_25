40d6c571ccb1c27b36f7f1d73e661fdb295c1499
Divya Davis <divyadavis01sdj@gmail.com>
2025-11-20 21:24:28 +0530

    pattern printing revise

--- Sample/sample_test/pattern_test.py
+++ Sample/sample_test/pattern_test.py
@@ -55,15 +55,70 @@
       
 #    print()
 
-num=1
-n=int(input("Enter a number :"))
-for i in range(1,n+1):
-   for j in range(1,n-i+1):
-      print(" ",end=" ")
-   for k in range(1,2*i):
-      print(num,end=" ")
-      k=k+1
-      num=num+1
-   print()
+# num=1
+# n=int(input("Enter a number :"))
+# for i in range(1,n+1):
+#    for j in range(1,n-i+1):
+#       print(" ",end=" ")
+#    for k in range(1,2*i):
+#       print(num,end=" ")
+#       k=k+1
+#       num=num+1
+#    print()
 
-   
+
+# """
+# 1
+# 2 1
+# 3 2 1
+# 4 3 2 1
+
+# """
+# n=int(input("Enter a num: "))
+# n=5
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(i,end=" ")
+#         i=i-1
+#     print()
+
+
+# """
+# 2
+# 3 5
+# 7 11 13
+# 17 19 23 29
+
+
+# """
+
+# n=50
+# count=0
+# l=[]
+# for j in range(2,n):
+#     for i in range(2,j):
+#         if(j%i == 0):
+#             break
+#     else:
+#         p=j
+#         l=l+[p]
+# c=0
+# for i in range(1,5+1):
+#     for k in range(i):
+#        print(l[c],end=" ")
+#        c=c+1
+#     print()
+
+# palindrome
+# s=input("enter a string: ")
+# if s==s[::-1]:
+#     print(f"{s} is panlindrome")
+# else:
+#     print("Not palinddrome")
+
+# s=int(input("Enter a nmber: "))
+# num=str(s)
+# if num==num[::-1]:
+#     print("palindrome")
+# else:
+#     print("not")
